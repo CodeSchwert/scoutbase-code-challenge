20c660ecf0023341ae733df41031c24b72781cfb
Nik Ho <studying.mole@gmail.com>
2019-09-02 17:35:22 +1200

    Move movie models to specific API version folder.

--- back-end/movie-data/imdb-data-files.js
+++ back-end/movie-data/imdb-data-files.js
@@ -11,10 +11,10 @@ const imdbDataFiles = [
   //   url: 'https://datasets.imdbws.com/title.akas.tsv.gz',
   //   filename: 'title.akas.tsv'
   // },
-  // {
-  //   url: 'https://datasets.imdbws.com/title.basics.tsv.gz',
-  //   filename: 'title.basics.tsv'
-  // },
+  {
+    url: 'https://datasets.imdbws.com/title.basics.tsv.gz',
+    filename: 'title.basics.tsv'
+  },
   // {
   //   url: 'https://datasets.imdbws.com/title.crew.tsv.gz',
   //   filename: 'title.crew.tsv'
@@ -23,10 +23,10 @@ const imdbDataFiles = [
   //   url: 'https://datasets.imdbws.com/title.episode.tsv.gz',
   //   filename: 'title.episode.tsv'
   // },
-  // {
-  //   url: 'https://datasets.imdbws.com/title.principals.tsv.gz',
-  //   filename: 'title.principals.tsv'
-  // },
+  {
+    url: 'https://datasets.imdbws.com/title.principals.tsv.gz',
+    filename: 'title.principals.tsv'
+  },
   // {
   //   url: 'https://datasets.imdbws.com/title.ratings.tsv.gz',
   //   filename: 'title.ratings.tsv'
