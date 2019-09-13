bc466889405b68f2c0d1a7abb93fbb4be63e2f12
Nik Ho <studying.mole@gmail.com>
2019-09-13 17:41:47 +1200

    Add login mutation.

--- back-end/src/controllers/v0/schema/mutation.ts
+++ back-end/src/controllers/v0/schema/mutation.ts
@@ -1,10 +1,13 @@
 import { GraphQLObjectType } from 'graphql';
-import createUser from './mutations/auth/createUser';
+// import createUser from './mutations/auth/createUser';
+// import login from './mutations/auth/login';
+import { createUser, login } from './mutations/auth';
 
 const Mutation: GraphQLObjectType = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    createUser
+    createUser,
+    login
   }
 });
 
--- back-end/src/controllers/v0/schema/mutations/auth/login.ts
+++ back-end/src/controllers/v0/schema/mutations/auth/login.ts
@@ -1,3 +1,51 @@
-import graphql from 'graphql';
-import { GraphQLString, GraphQLNonNull } from 'graphql';
+import { GraphQLNonNull, GraphQLString } from 'graphql';
+import * as bcrypt from 'bcrypt';
+import UserType from '../../types/users/user';
+import { User } from '../../../../../server';
+import { generateJWT } from '../../../services/auth';
 
+export default {
+  type: UserType,
+  args: {
+    username: { type: new GraphQLNonNull(GraphQLString) },
+    password: { type: new GraphQLNonNull(GraphQLString) }
+  },
+  async resolve(parentValue: any, args: any, context: any) {
+    try {    
+      const { username, password } = args;
+
+      /**
+       * TODO - check username and password for reasonable values
+       */
+
+      // @ts-ignore
+      const user = await User.findOne({
+        where: { username }
+      });
+
+      if (user) {
+        const hash = user.password;
+        console.log('hash', hash)
+        const passwordsMatch = await bcrypt.compare(password, hash);
+        console.log('passwordsMatch', passwordsMatch);
+
+        if (!passwordsMatch) {
+          context.res.clearCookie('scoutbase-code-challenge');
+          context.res.status(401); // wrong password
+          
+          return { username: '' };
+        } else {
+          const jwt = generateJWT(username);
+          context.res.cookie('scoutbase-code-challenge', jwt, { httpOnly: true });
+  
+          return { username };
+        }
+      } else {
+        context.res.status(401); // username not found
+        return { username: '' };
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
